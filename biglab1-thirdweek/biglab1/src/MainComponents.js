4f307e859189313e9cb89da19fa591dce2b3f0ef
ivanbrancati-s280170 <s280170@studenti.polito.it>
2021-04-30 11:08:46 +0200

    Added fake edit task

--- biglab1-thirdweek/biglab1/src/MainComponents.js
+++ biglab1-thirdweek/biglab1/src/MainComponents.js
@@ -24,7 +24,7 @@ const TaskRow = (props) => {
                 <div className="d-flex w-100 justify-content-between pt-1">
                     <TaskInfo className="d-flex w-75" {...props}/>
                     <div className="d-flex w-25 justify-content-end">
-                        <TaskControls id={props.task.id} removeTask={props.removeTask}/>
+                        <TaskControls id={props.task.id} removeTask={props.removeTask} editTask={props.editTask}/>
                     </div>
                 </div>
             </ListGroup.Item>) ;
@@ -66,8 +66,8 @@ const TaskInfo = (props) => {
 const TaskControls = (props) => {
     return (
             <p>
-                {/*TODO: onclick for editing task*/}
-                <svg onClick={()=>console.log("Click")} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-fill text-warning" viewBox="0 0 16 16">
+                {/*TODO: improve editing task*/}
+                <svg onClick={()=>props.editTask(props.id, "Edited", true, true, undefined)} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-fill text-warning" viewBox="0 0 16 16">
                     <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
                 </svg>
                 <svg onClick={()=>props.removeTask(props.id)}xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash text-danger" viewBox="0 0 16 16">
@@ -82,7 +82,7 @@ const TaskControls = (props) => {
 const ToDoTaskList = (props) => {
     return (
             <ListGroup as="ul" variant="flush" className="tasklist">
-                { props.elements.map( (e) => <TaskRow task={e} key={e.id} removeTask={props.removeTask}/>)}
+                { props.elements.map( (e) => <TaskRow task={e} key={e.id} removeTask={props.removeTask} editTask={props.editTask}/>)}
             </ListGroup>
             ) ;
 } ;
@@ -236,7 +236,7 @@ const ToDoMain = (props) => {
             <Col as='main' xs={12} sm={8}>
                 <div id="tasklist-container">
                     <FilterTitle title={props.title}></FilterTitle>   
-                    <ToDoTaskList elements={props.tasks} removeTask={props.removeTask}></ToDoTaskList>
+                    <ToDoTaskList elements={props.tasks} removeTask={props.removeTask} editTask={props.editTask}></ToDoTaskList>
                     <AddButton showModal={showModal} openModal={openModal}></AddButton>
                     <AddModal showModal={showModal} closeModal={closeModal} addTask={props.addTask}></AddModal>
                 </div>
