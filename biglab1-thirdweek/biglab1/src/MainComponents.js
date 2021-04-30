00bb3ff02914af7f14537029fc3194b53aa75dc7
ivanbrancati-s280170 <s280170@studenti.polito.it>
2021-04-30 10:40:12 +0200

    Task removal works correctly

--- biglab1-thirdweek/biglab1/src/MainComponents.js
+++ biglab1-thirdweek/biglab1/src/MainComponents.js
@@ -23,9 +23,8 @@ const TaskRow = (props) => {
     return (<ListGroup.Item className="tasklist-elem mylistmain" key={props.task.id}>
                 <div className="d-flex w-100 justify-content-between pt-1">
                     <TaskInfo className="d-flex w-75" {...props}/>
-
                     <div className="d-flex w-25 justify-content-end">
-                    <TaskControls/>
+                        <TaskControls id={props.task.id} removeTask={props.removeTask}/>
                     </div>
                 </div>
             </ListGroup.Item>) ;
@@ -67,11 +66,11 @@ const TaskInfo = (props) => {
 const TaskControls = (props) => {
     return (
             <p>
-                {/*TODO: onclick*/}
+                {/*TODO: onclick for editing task*/}
                 <svg onClick={()=>console.log("Click")} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-fill text-warning" viewBox="0 0 16 16">
                     <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
                 </svg>
-                <svg onClick={()=>console.log("ClickDelete")}xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash text-danger" viewBox="0 0 16 16">
+                <svg onClick={()=>props.removeTask(props.id)}xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash text-danger" viewBox="0 0 16 16">
                     <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                     <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
                 </svg>
@@ -83,7 +82,7 @@ const TaskControls = (props) => {
 const ToDoTaskList = (props) => {
     return (
             <ListGroup as="ul" variant="flush" className="tasklist">
-                { props.elements.map( (e) => <TaskRow task={e} key={e.id}/>)}
+                { props.elements.map( (e) => <TaskRow task={e} key={e.id} removeTask={props.removeTask}/>)}
             </ListGroup>
             ) ;
 } ;
@@ -237,7 +236,7 @@ const ToDoMain = (props) => {
             <Col as='main' xs={12} sm={8}>
                 <div id="tasklist-container">
                     <FilterTitle title={props.title}></FilterTitle>   
-                    <ToDoTaskList elements={props.tasks}></ToDoTaskList>
+                    <ToDoTaskList elements={props.tasks} removeTask={props.removeTask}></ToDoTaskList>
                     <AddButton showModal={showModal} openModal={openModal}></AddButton>
                     <AddModal showModal={showModal} closeModal={closeModal} addTask={props.addTask}></AddModal>
                 </div>
