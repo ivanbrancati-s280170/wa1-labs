d545c0df65e8563044d693d2ebf5d2eb7220d230
ivanbrancati-s280170 <s280170@studenti.polito.it>
2021-05-01 17:48:25 +0200

    Added some changes (still to fix)

--- biglab1-thirdweek/biglab1/src/MainComponents.js
+++ biglab1-thirdweek/biglab1/src/MainComponents.js
@@ -1,4 +1,7 @@
 //TODO: sistemare 'scorrimento' pagina
+//TODO: order in states
+//TODO: coherence urgent/important
+//TODO: component uncontrolled -> controlled (warning)
 import { useState } from 'react';
 import {ListGroup, Col, Form, Modal, Button} from 'react-bootstrap' ;
 //import dayjs from 'dayjs' ;
@@ -27,7 +30,7 @@ const TaskRow = (props) => {
                 <div className="d-flex w-100 justify-content-between pt-1">
                     <TaskInfo className="d-flex w-75" {...props}/>
                     <div className="d-flex w-25 justify-content-end">
-                        <TaskControls id={props.task.id} removeTask={props.removeTask} openModal={props.openModal} handleModalMode={props.handleModalMode} editTask={props.editTask}/>
+                        <TaskControls id={props.task.id} removeTask={props.removeTask} openModal={props.openModal} handleModalMode={props.handleModalMode} handleToEdit={props.handleToEdit} editTask={props.editTask}/>
                     </div>
                 </div>
             </ListGroup.Item>) ;
@@ -72,8 +75,8 @@ const TaskControls = (props) => {
                 {/*TODO: improve editing task*/}
                 <svg onClick={()=>{
                                     props.handleModalMode("edit") ;
+                                    props.handleToEdit(props.id) ;
                                     props.openModal() ;
-                                    props.editTask(props.id, "Edited", true, true, undefined)
                                 }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-fill text-warning" viewBox="0 0 16 16">
                     <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
                 </svg>
@@ -89,7 +92,7 @@ const TaskControls = (props) => {
 const ToDoTaskList = (props) => {
     return (
             <ListGroup as="ul" variant="flush" className="tasklist">
-                { props.elements.map( (e) => <TaskRow task={e} key={e.id} removeTask={props.removeTask} openModal={props.openModal} handleModalMode={props.handleModalMode} editTask={props.editTask}/>)}
+                { props.elements.map( (e) => <TaskRow task={e} key={e.id} removeTask={props.removeTask} openModal={props.openModal} handleModalMode={props.handleModalMode} handleToEdit={props.handleToEdit} editTask={props.editTask}/>)}
             </ListGroup>
             ) ;
 } ;
@@ -106,8 +109,8 @@ const AddButton = (props) => {
 
 const AddModal = (props) => {
     const [description, setDescription] = useState('') ;
-    const [deadlineDate, setDeadlineDate] = useState(undefined) ;
-    const [deadlineTime, setDeadlineTime] = useState(undefined) ;
+    const [deadlineDate, setDeadlineDate] = useState('') ;
+    const [deadlineTime, setDeadlineTime] = useState('') ;
     const [privacy, setPrivacy] = useState(true) ;
     const [important, setImportant] = useState(false) ;
     const [deadlineInput, setDeadlineInput] = useState(false) ;
@@ -142,7 +145,7 @@ const AddModal = (props) => {
         //TODO:VALIDATION
         if (description_validity && deadlineDate_validity && deadlineTime_validity) {
         const new_task = {description: description, deadline: deadlineInput && `${deadlineDate} ${deadlineTime}`, privacy:privacy, urgent: important}
-        props.addTask(new_task) ;
+        props.modalMode === "create"?props.addTask(new_task):props.editTask(props.taskToEdit.id, new_task.description, new_task.urgent, new_task.privacy, new_task.deadline) ;
         props.closeModal() ;
         resetForms() ;
         } ;
@@ -162,8 +165,23 @@ const AddModal = (props) => {
         setDeadlineTimeValidity(true) ;
     } ;
 
+    const fillForms = () => {
+        //const task = props.taskToEdit ;
+        setDescription(props.taskToEdit.description) ;
+        if(props.taskToEdit.deadline)
+            {
+                setDeadlineInput(true) ;
+                setDeadlineDate(props.taskToEdit.deadline.format("YYYY-MM-DD")) ;
+                setDeadlineTime(props.taskToEdit.deadline.format("HH:mm")) ;
+            } 
+        else setDeadlineInput(false) ;
+        setPrivacy(props.taskToEdit.privacy) ;
+        setImportant(props.taskToEdit.urgent) ;
+        
+    } ;
+
     return (
-            <Modal /*animation={false}*/ show={props.showModal} /*TODO: correct or not?*/onHide={() => { props.closeModal(); resetForms(); }} 
+            <Modal /*animation={false}*/ show={props.showModal} /*TODO: correct or not?*/onHide={() => { props.closeModal(); resetForms(); }} onShow={() => props.modalMode === "edit"?fillForms():""}
             size="lg"
             aria-labelledby="contained-modal-title-vcenter"
             centered>
@@ -244,14 +262,19 @@ const ToDoMain = (props) => {
     //function to change the modal mode
     const handleModalMode = (newModalMode) => setModalMode(() => newModalMode) ;
 
+    //state to manage the task to edit 
+    const [taskToEdit, setTaskToEdit] = useState('') ;
+
+    //function to set the task to edit
+    const handleToEdit = (id) => setTaskToEdit(() => props.tasks.filter((task) => task.id === id)[0]) ;
 
     return (
             <Col as='main' xs={12} sm={8}>
                 <div id="tasklist-container">
                     <FilterTitle title={props.title}></FilterTitle>   
-                    <ToDoTaskList elements={props.tasks} removeTask={props.removeTask} openModal={openModal} handleModalMode={handleModalMode} editTask={props.editTask}></ToDoTaskList>
+                    <ToDoTaskList elements={props.tasks} removeTask={props.removeTask} openModal={openModal} handleModalMode={handleModalMode} handleToEdit={handleToEdit}></ToDoTaskList>
                     <AddButton showModal={showModal} openModal={openModal} modalMode={modalMode} handleModalMode={handleModalMode}></AddButton>
-                    <AddModal showModal={showModal} modalMode={modalMode} closeModal={closeModal} handleModalMode={handleModalMode} addTask={props.addTask}></AddModal>
+                    <AddModal showModal={showModal} modalMode={modalMode} closeModal={closeModal} handleModalMode={handleModalMode} addTask={props.addTask} editTask={props.editTask} elements={props.tasks} taskToEdit={taskToEdit}></AddModal>
                 </div>
             </Col>
             ) ;
